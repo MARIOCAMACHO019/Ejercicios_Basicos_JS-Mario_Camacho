75e3c78f27426ae0612c8fa2e0b9fa3ba41fa72e
Mario Camacho Prieto <mario.claserebeca@gmail.com>
2025-08-27 00:56:45 +0200

    Create ejercicio3.js

---
+++ ejercicio3.js
@@ -0,0 +1,20 @@
+// 3.1 Multiplica 10 por 5
+console.log(10 * 5);
+
+// 3.2 Divide 10 por 2
+console.log(10 / 2);
+
+// 3.3 Muestra el resto de dividir 15 por 9
+console.log(15 % 9);
+
+// 3.4 Operador de asignación para o = 15
+let p = 10;
+let j = 5;
+let o = p + j;
+console.log(o);
+
+// 3.5 Operador de asignación para i = 50
+let c = 10;
+let m = 5;
+let i = c * m;
+console.log(i);
