b4637e6e202720aad521d809872774943840ae0f
Mario Camacho Prieto <mario.claserebeca@gmail.com>
2025-08-27 00:56:42 +0200

    Create ejercicio2.js

---
+++ ejercicio2.js
@@ -0,0 +1,16 @@
+// 1. Luke Skywalker cumple años:
+const jedi = {nombre: "Luke Skywalker", edad: 19};
+jedi.edad = 25;
+console.log(jedi);
+
+// 2. Presentación al estilo Leia Organa:
+const nombre = "Leia";
+const apellido = "Organa";
+const edad = 20;
+console.log("Soy " + nombre + " " + apellido + ", tengo " + edad + " años y soy una princesa de Alderaan.");
+
+// 3. Calculando el coste total de sables de luz:
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+const precioTotalSables = sable1.precio + sable2.precio;
+console.log(precioTotalSables);
